4fcc066f40fffca99ba9096a4aef97bde3b9fa10
Josh Balch <Joshrbalch@gmail.com>
2024-09-10 17:48:08 -0500

    Changes

--- Fall2024-Assignment1-jrbalch/wwwroot/js/site.js
+++ Fall2024-Assignment1-jrbalch/wwwroot/js/site.js
@@ -1,4 +1,17 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+playStop = 0;
+let audio = new Audio('kung.mp3');
+function playSong() {
+    if (playStop == 0) {
+        audio.play();
+        playStop = 1;
+    }
+
+    else {
+        audio.pause();
+        playStop = 0;
+    }
+
+}
